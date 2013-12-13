15ad33da5bf5142274e3cc2c69781a3176bc1752
Florian Eggenhofer <egg@tbi.univie.ac.at>
2013-12-13 16:53:49 +0100

    Added NCBI taxdump datastructures

--- src/Bio/TaxonomyData.hs
+++ src/Bio/TaxonomyData.hs
@@ -1,6 +1,5 @@
--- | This module contains a hierarchical data structure for
+-- | This module contains data structures for
 --   taxonomy data
-
 
 module Bio.TaxonomyData where
     
@@ -11,3 +10,85 @@ data Taxonomy = Taxonomy
   }
   deriving (Show, Eq)
 
+data NCBITaxDump = Taxonomy
+  {
+    TaxDumpCitations :: [TaxDumpCitation],
+    TaxDumpDelNodes :: [TaxDumpDelNode],
+    TaxDumpDivisions :: [TaxDumpDivison],
+    TaxDumpGencodes :: [TaxDumpGencode],
+    TaxDumpMergedNodes :: [TaxDumpMergedNode],
+    TaxDumpNames :: [TaxDumpName],
+    TaxDumpNodes :: [TaxDumpNode]
+  }
+  deriving (Show, Eq)
+
+data TaxDumpCitation = Taxonomy
+  {
+   citId :: String,
+   citKey :: String,
+   pubmedId :: String,
+   medlineId :: String,
+   url :: String,
+   text :: String,
+   taxidList :: [String]
+  }
+  deriving (Show, Eq)
+
+data TaxDumpDelNode = Taxonomy
+  {
+   taxID :: String
+  }
+  deriving (Show, Eq)
+
+data TaxDumpDivision = Taxonomy
+  {
+   divisionId :: String,
+   divisionCDE :: String,
+   divisonName :: String,
+   comments :: String
+  }
+  deriving (Show, Eq)
+
+data TaxDumpGencode = Taxonomy
+  {
+   geneticCodeId :: String,
+   abbreviation :: String,
+   name :: String,
+   cde :: String,
+   starts :: String
+  }
+  deriving (Show, Eq)
+
+data TaxDumpMergedNode = Taxonomy
+  {
+   oldTaxId :: String,
+   newTaxId :: String
+  }
+  deriving (Show, Eq)
+
+data TaxDumpName = Taxonomy
+  {
+   taxID :: String,
+   nameTxt :: String,
+   uniqueName :: String,
+   nameClass :: String
+  }
+  deriving (Show, Eq)
+
+data TaxDumpNode = Taxonomy
+  {
+   taxId :: String,
+   parentTaxId :: String,
+   rank :: String,
+   emblCode :: String,
+   divisionId :: String,
+   inheritiedDivFlag :: String,
+   geneticCodeId :: String,
+   inheritedGCFlag :: String,
+   mitochondrialGeneticCodeId :: String,
+   inheritedMGCFlag :: String,
+   genBankHiddenFlag :: String,
+   hiddenSubtreeRootFlag :: String,
+   comments :: String
+  }
+  deriving (Show, Eq)
