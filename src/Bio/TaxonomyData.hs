46bc3849c459d9da0f5f9169d5554c408f9c2e1e
Florian Eggenhofer <egg@tbi.univie.ac.at>
2013-12-30 14:11:09 +0100

    Integrated description from taxdump readme as comments in datatypes and refined types accordingly

--- src/Bio/TaxonomyData.hs
+++ src/Bio/TaxonomyData.hs
@@ -3,13 +3,15 @@
 
 module Bio.TaxonomyData where
     
--- | RNAplex output consists of a set of interactions 
+-- | 
 data Taxonomy = Taxonomy
   {
     taxData :: String
   }
   deriving (Show, Eq)
 
+-- | NCBI Taxonomy database dump hierachichal data structure
+-- as defined in ftp://ftp.ncbi.nih.gov/pub/taxonomy/taxdump_readme.txt
 data NCBITaxDump = Taxonomy
   {
     TaxDumpCitations :: [TaxDumpCitation],
@@ -24,26 +26,42 @@ data NCBITaxDump = Taxonomy
 
 data TaxDumpCitation = Taxonomy
   {
+   -- the unique id of citation
    citId :: String,
+   -- citation key
    citKey :: String,
-   pubmedId :: String,
-   medlineId :: String,
+   -- unique id in PubMed database (0 if not in PubMed)
+   pubmedId :: Maybe String,
+   -- unique id in MedLine database (0 if not in MedLine)
+   medlineId :: Maybe String,
+   -- URL associated with citation
    url :: String,
+   -- any text (usually article name and authors)
+   -- The following characters are escaped in this text by a backslash:
+						-- newline (appear as "\n"),
+						-- tab character ("\t"),
+						-- double quotes ('\"'),
+						-- backslash character ("\\").
    text :: String,
+   -- list of node ids separated by a single space
    taxidList :: [String]
   }
   deriving (Show, Eq)
 
 data TaxDumpDelNode = Taxonomy
   {
-   taxID :: String
+   -- deleted node id
+   taxID :: Int
   }
   deriving (Show, Eq)
 
 data TaxDumpDivision = Taxonomy
   {
+   -- taxonomy database division id
    divisionId :: String,
+   -- GenBank division code (three characters)
    divisionCDE :: String,
+   -- e.g. BCT, PLN, VRT, MAM, PRI...
    divisonName :: String,
    comments :: String
   }
@@ -51,44 +69,68 @@ data TaxDumpDivision = Taxonomy
 
 data TaxDumpGencode = Taxonomy
   {
+   -- GenBank genetic code id
    geneticCodeId :: String,
+   -- genetic code name abbreviation
    abbreviation :: String,
+   -- genetic code name
    name :: String,
+   -- translation table for this genetic code
    cde :: String,
+   -- start codons for this genetic code
    starts :: String
   }
   deriving (Show, Eq)
 
 data TaxDumpMergedNode = Taxonomy
   {
-   oldTaxId :: String,
-   newTaxId :: String
+   -- id of nodes which has been merged
+   oldTaxId :: Int,
+   -- id of nodes which is result of merging
+   newTaxId :: Int
   }
   deriving (Show, Eq)
 
 data TaxDumpName = Taxonomy
   {
-   taxID :: String,
+   -- the id of node associated with this name
+   taxID :: Int,
+   -- name itself
    nameTxt :: String,
+   -- the unique variant of this name if name not unique
    uniqueName :: String,
+   -- (synonym, common name, ...)
    nameClass :: String
   }
   deriving (Show, Eq)
 
 data TaxDumpNode = Taxonomy
   {
-   taxId :: String,
-   parentTaxId :: String,
+   -- node id in GenBank
+   taxId :: Int,
+   -- parent node id in GenBank taxonomy database
+   parentTaxId :: Int,
+   -- rank of this node (superkingdom, kingdom, ...) 
    rank :: String,
+   -- locus-name prefix; not unique
    emblCode :: String,
+   -- see division.dmp file
    divisionId :: String,
-   inheritiedDivFlag :: String,
+   -- 1 if node inherits division from parent
+   inheritiedDivFlag :: Bool,
+   -- see gencode.dmp file
    geneticCodeId :: String,
-   inheritedGCFlag :: String,
+   -- 1 if node inherits genetic code from parent
+   inheritedGCFlag :: Bool,
+   -- see gencode.dmp file 
    mitochondrialGeneticCodeId :: String,
-   inheritedMGCFlag :: String,
-   genBankHiddenFlag :: String,
-   hiddenSubtreeRootFlag :: String,
+   -- 1 if node inherits mitochondrial gencode from parent
+   inheritedMGCFlag :: Bool,
+   -- 1 if name is suppressed in GenBank entry lineage
+   genBankHiddenFlag :: Boolean,
+   -- 1 if this subtree has no sequence data yet
+   hiddenSubtreeRootFlag :: Bool,
+   -- free-text comments and citations
    comments :: String
   }
   deriving (Show, Eq)
